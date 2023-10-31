45154edc7032152ee3ae6890944bad276ee52fe0
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-10-31 15:18:53 -0300

    valid email e senha

--- app/backend/src/model/Users.model.ts
+++ app/backend/src/model/Users.model.ts
@@ -8,6 +8,8 @@ export default class UsersModel implements IUsersModel {
   private model = SequelizeUsers;
 
   async login({ email, password }: IUserLogin) : Promise<ServiceResponse<Token | ServiceMessage>> {
+    const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+
     if (!email || !password) {
       return { status: 'badRequest',
         data: { message: 'All fields must be filled' } };
@@ -15,7 +17,8 @@ export default class UsersModel implements IUsersModel {
 
     const foundUser = await this.model.findOne({ where: { email } });
 
-    if (!foundUser || !compareSync(password, foundUser.dataValues.password)) {
+    if (!foundUser || !emailRegex.test(email) || password.length < 6
+    || !compareSync(password, foundUser.dataValues.password)) {
       return { status: 'unauthorized', data: { message: 'Invalid email or password' } };
     }
 
