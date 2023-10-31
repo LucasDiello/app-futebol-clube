55419d558d5d4d093c8673750552c735613f5a23
Lucas Diello <Lucasoliveiradiello@hotmail.com>
2023-10-31 11:37:17 -0300

    add migration para users

---
+++ app/backend/src/database/migrations/02-create-users.ts
@@ -0,0 +1,44 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import IUsers from '../../Interfaces/IUsers';
+
+
+export default {
+    up(QueryInterface: QueryInterface) {
+        return QueryInterface.createTable<Model<IUsers>>('users', {
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: 'username',
+            },
+
+            role: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: 'role',
+            },
+
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: 'email',
+            },
+
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                field: 'password',
+            },
+
+        })
+    },
+    down(QueryInterface: QueryInterface) {
+        return QueryInterface.dropTable('users');
+    }
+}
